f727dace4f9b5f9de75046bfd832c7cc554168c1
PoojaKalange <pujacompu@gmail.com>
2016-01-21 18:24:03 +0530

    Change BMS
    Service api

--- public/js/controller/editEventsController.js
+++
@@ -1,9 +0,0 @@
-app.controller('editEventsController',function($scope,$location,$firebaseArray){
-    
-    var eventref = new Firebase("https://sportsdataevent.firebaseio.com/event");
-
-  // create a synchronized array
-  $scope.event = $firebaseArray(eventref);
-    
-    $scope.msg="Pooja";
-});
